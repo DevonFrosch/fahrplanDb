93f72a117d3dbefba8dc6872aff0024eb6b83df6
DevonFrosch <git@devonfrosch.eu>
2022-02-06 12:51:04 +0100

    Added current commit info to admin front page

--- src/admin/index.php
+++ src/admin/index.php
@@ -10,5 +10,12 @@
 			<li><a href="../">Startseite</a></li>
 			<li><a href="import.php">Import</a></li>
 		</ul>
+		<p>
+			Git-Commit:
+			<pre><?php
+				exec('git show HEAD -s --format="%h%d%n%ci%n%s"', $output);
+				echo htmlspecialchars(join("\n", $output));
+			?></pre>
+		</p>
 	</body>
 </html>
